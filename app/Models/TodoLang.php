ddbefcab9401ced9996357f3b2c3577e2a6f7aa8
berndengels <engels@f50.de>
2021-05-05 15:23:29 +0200

    stuff

---
+++ app/Models/TodoLang.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TodoLang extends Model
+{
+    protected $table = 'todos_lang';
+
+    public function test(){
+        // aktuelle sprache ermittlen
+        $lang = App::getLocale();
+    }
+}
