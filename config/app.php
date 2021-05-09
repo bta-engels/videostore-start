08e74d450337db55da5583ae91030db9c556f560
berndengels <engels@f50.de>
2021-05-09 15:26:01 +0200

    translation

--- config/app.php
+++ config/app.php
@@ -221,6 +221,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // extras
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
