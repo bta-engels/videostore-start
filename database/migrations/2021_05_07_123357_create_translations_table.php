985ef397371d816c41b52c871d120e4e8549f247
berndengels <engels@f50.de>
2021-05-09 18:05:52 +0200

    stuff

--- database/migrations/2021_05_07_123357_create_translations_table.php
+++ database/migrations/2021_05_07_123357_create_translations_table.php
@@ -14,12 +14,13 @@ class CreateTranslationsTable extends Migration
     public function up()
     {
         Schema::create('translations', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->unsignedInteger('translatable_id')->index();
             $table->string('translatable_type', 50);
             $table->string('language',2)->index();
             $table->json('content');
             $table->timestamps();
+            $table->unique(['translatable_type', 'translatable_id', 'language']);
         });
     }
 
