068128fc83cbcf24c4d42826bdd411d635874072
berndengels <engels@f50.de>
2021-05-12 15:40:29 +0200

    add create_at to TodoResource fpr API Request

--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,10 +16,14 @@ class TodoResource extends JsonResource
      */
     public function toArray($request)
     {
+        /**
+         * @var Todo $this
+         */
         return [
             'id'    => $this->id,
             'done'  => $this->done,
             'text'  => $this->text,
+            'created_at' => $this->created_at->format('d.m.Y H:i'),
         ];
     }
 }
